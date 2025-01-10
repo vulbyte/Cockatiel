e04e72c167ee9d9e3ba4672cf848c1e253e02539
vulbyte <vulbyt3@gmail.com>
2025-01-10 15:28:07 -0800

    qgit @: 2025-01-10 15:28:05, from user vulbyte

---
+++ lib/c_json_parser_for_wasm.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+
+char *allocate_memory(size_t size) {
+	return(char *)malloc(size);
+}
+
+char *process_string(const char *input){
+	size_t len = strlen(input);
+
+	char result; /*= 'thing'*/
+	
+	return result;
+}
+
+int main(){
+	return 0;
+}
